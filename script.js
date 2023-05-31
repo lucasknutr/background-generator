af8bdf061c02a3a512d46d8e721aae72c9f48e88
Lucas <lucasclaudinocc@gmail.com>
2023-05-31 18:45:09 -0300

    Added code input for CSS usage

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 let body = document.getElementById("gradient");
 let color1 = document.getElementById("color1");
 let color2 = document.getElementById("color2");
-let h3 = document.querySelector("h3");
+let textArea = document.querySelector("#css-input");
+let leftButton = document.querySelector("#left");
+let rightButton = document.querySelector("#right");
+let direction;
+
+leftButton.addEventListener("click", function () {
+    return direction = "to left";
+})
+
+rightButton.addEventListener("click", function () {
+    return direction = "to right";
+})
+
+
+let side = function () {
+    if (direction === "to left") {
+        return "to left";
+    }
+    else {
+        return "to right";
+    }
+}
+
+
 
 body.addEventListener("input", function() {
-    body.style.background = "linear-gradient(" + "to right" + ", " + color1.value + ", " + color2.value + ")";
+    body.style.background = "linear-gradient(" + side() + ", " + color1.value + ", " + color2.value + ")";
 
-    h3.textContent = body.style.background;
+    textArea.textContent = "background: " + body.style.background;
 })
 
 
